2dc44675827a0961b5fb40fe1d1638fef9605ff5
André Luís <zandreluisos@gmail.com>
2025-08-19 19:45:33 -0300

    Move commands one level up

--- src/main/java/io/github/syntaxpresso/core/command/RenameCommand.java
+++ src/main/java/io/github/syntaxpresso/core/command/RenameCommand.java
@@ -1,13 +1,15 @@
-package io.github.syntaxpresso.core.command.java;
+package io.github.syntaxpresso.core.command;
 
-import io.github.syntaxpresso.core.command.java.dto.RenameResponse;
-import io.github.syntaxpresso.core.command.java.extra.SourceDirectoryType;
+import io.github.syntaxpresso.core.command.dto.RenameResponse;
+import io.github.syntaxpresso.core.command.extra.SourceDirectoryType;
 import io.github.syntaxpresso.core.common.DataTransferObject;
 import io.github.syntaxpresso.core.service.java.JavaService;
 import java.io.File;
+import java.nio.file.Path;
 import java.util.concurrent.Callable;
 import lombok.RequiredArgsConstructor;
 import picocli.CommandLine;
+import picocli.CommandLine.Option;
 
 @CommandLine.Command(
     name = "rename",
@@ -16,6 +18,9 @@ import picocli.CommandLine;
 @RequiredArgsConstructor
 public class RenameCommand implements Callable<DataTransferObject<RenameResponse>> {
   private final JavaService javaService;
+
+  @Option(names = "--cwd", description = "Current Working Directory", required = true)
+  private Path cwd;
 
   @CommandLine.Option(
       names = {"-f", "--file-path"},
@@ -28,6 +33,18 @@ public class RenameCommand implements Callable<DataTransferObject<RenameResponse
       description = "The new name for the class/interface/enum.",
       required = true)
   private String newName;
+
+  @CommandLine.Option(
+      names = {"-l", "--line"},
+      description = "The cursor line",
+      required = true)
+  private Integer line;
+
+  @CommandLine.Option(
+      names = {"-c", "--column"},
+      description = "The cursor column",
+      required = true)
+  private Integer column;
 
   @CommandLine.Option(
       names = {"-s", "--source-directory"},
