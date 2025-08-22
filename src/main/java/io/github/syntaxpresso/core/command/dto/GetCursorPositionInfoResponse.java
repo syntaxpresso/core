88991cc81d479be8cbc01e214226e0e5bb129f93
André Luís <zandreluisos@gmail.com>
2025-08-22 20:22:03 -0300

    Add language field to GetCursorPositionInfoResponse
    - Add SupportedLanguage field to GetCursorPositionInfoResponse DTO
    - Populate language field with SupportedLanguage.JAVA in JavaService
    - Provides language context for cursor position information responses

--- src/main/java/io/github/syntaxpresso/core/command/dto/GetCursorPositionInfoResponse.java
+++ src/main/java/io/github/syntaxpresso/core/command/dto/GetCursorPositionInfoResponse.java
@@ -1,5 +1,6 @@
 package io.github.syntaxpresso.core.command.dto;
 
+import io.github.syntaxpresso.core.common.extra.SupportedLanguage;
 import io.github.syntaxpresso.core.service.extra.JavaIdentifierType;
 import java.io.Serializable;
 import lombok.*;
@@ -10,6 +11,7 @@ import lombok.*;
 @Builder
 public class GetCursorPositionInfoResponse implements Serializable {
   private String filePath;
+  private SupportedLanguage language;
   private String node;
   private JavaIdentifierType nodeType;
   private String nodeText;
