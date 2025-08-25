438c90026885b45d51c7688b342f7a657498c2af
André Luís <zandreluisos@gmail.com>
2025-08-25 15:09:26 -0300

    Create GetJPAEntityInfoCommand

---
+++ src/main/java/io/github/syntaxpresso/core/command/GetJPAEntityInfoCommand.java
@@ -0,0 +1,40 @@
+package io.github.syntaxpresso.core.command;
+
+import io.github.syntaxpresso.core.command.dto.GetJPAEntityInfoResponse;
+import io.github.syntaxpresso.core.common.DataTransferObject;
+import io.github.syntaxpresso.core.common.extra.SupportedIDE;
+import io.github.syntaxpresso.core.service.java.JavaService;
+import java.nio.file.Path;
+import java.util.concurrent.Callable;
+import lombok.RequiredArgsConstructor;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@RequiredArgsConstructor
+@Command(
+    name = "get-jpa-entity-info",
+    description = "Get necessary info of an specific Entity to create a JPA repository.")
+public class GetJPAEntityInfoCommand
+    implements Callable<DataTransferObject<GetJPAEntityInfoResponse>> {
+  private final JavaService javaService;
+
+  @Option(names = "--cwd", description = "Current Working Directory", required = true)
+  private Path cwd;
+
+  @Option(
+      names = {"--file-path"},
+      description = "The absolute path to the .java file.",
+      required = true)
+  private Path filePath;
+
+  @Option(
+      names = "--ide",
+      description = "The IDE the command is being called from.",
+      required = true)
+  private SupportedIDE ide = SupportedIDE.NONE;
+
+  @Override
+  public DataTransferObject<GetJPAEntityInfoResponse> call() {
+    return this.javaService.getJPAEntityInfo(this.cwd, this.filePath, this.ide);
+  }
+}
