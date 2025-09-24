8d4afa734ed5a5cfa1289d33cde97f2383c0243f
André Luís <zandreluisos@gmail.com>
2025-09-24 17:58:13 -0300

    Remove unnecessary JavaFileTemplate

--- src/main/java/io/github/syntaxpresso/core/command/extra/JavaFileTemplate.java
+++ src/main/java/io/github/syntaxpresso/core/command/extra/JavaFileTemplate.java
@@ -5,8 +5,7 @@ public enum JavaFileTemplate {
   INTERFACE("package %s;%n%npublic interface %s {\n\n}"),
   ENUM("package %s;%n%npublic enum %s {\n\n}"),
   RECORD("package %s;%n%npublic record %s(\n\n) {\n\n}"),
-  ANNOTATION("package %s;%n%npublic @interface %s {\n\n}"),
-  JPA_REPOSITORY("package %s;%n%nimport org.springframework.data.jpa.repository.JpaRepository;%nimport org.springframework.stereotype.Repository;%n%n@Repository%npublic interface %sRepository extends JpaRepository<%s, %s> {%n%n}");
+  ANNOTATION("package %s;%n%npublic @interface %s {\n\n}");
   private final String template;
 
   JavaFileTemplate(String template) {
@@ -17,10 +16,8 @@ public enum JavaFileTemplate {
     return String.format(this.template, packageName, className);
   }
 
-  public String getSourceContent(String packageName, String className, String entityType, String idType) {
-    if (this == JPA_REPOSITORY) {
-      return String.format(this.template, packageName, entityType, entityType, idType);
-    }
+  public String getSourceContent(
+      String packageName, String className, String entityType, String idType) {
     return String.format(this.template, packageName, className);
   }
 }
