140ab2c4a061bcba7db0098b00b060bf32092525
André Luís <zandreluisos@gmail.com>
2025-10-01 10:43:32 -0300

    Add GetJPAEntityInfoCommand and DTO implementation
    - GetJPAEntityInfoCommand: Picocli command with --cwd, --file-path, --language, --ide, --superclass-source parameters
    - GetJPAEntityInfoResponse: DTO with entity metadata (isJPAEntity, entityType, entityPackageName, entityPath, idFieldType, idFieldPackageName)
    - Supports Java language with external superclass analysis capability

--- src/main/java/io/github/syntaxpresso/core/command/GetJPAEntityInfoCommand.java
+++ src/main/java/io/github/syntaxpresso/core/command/GetJPAEntityInfoCommand.java
@@ -1,40 +1,57 @@
-// package io.github.syntaxpresso.core.command;
-//
-// import io.github.syntaxpresso.core.command.dto.GetJPAEntityInfoResponse;
-// import io.github.syntaxpresso.core.common.DataTransferObject;
-// import io.github.syntaxpresso.core.common.extra.SupportedIDE;
-// import io.github.syntaxpresso.core.service.java.JavaCommandService;
-// import java.nio.file.Path;
-// import java.util.concurrent.Callable;
-// import lombok.RequiredArgsConstructor;
-// import picocli.CommandLine.Command;
-// import picocli.CommandLine.Option;
-//
-// @RequiredArgsConstructor
-// @Command(
-//     name = "get-jpa-entity-info",
-//     description = "Get necessary info of an specific Entity to create a JPA repository.")
-// public class GetJPAEntityInfoCommand
-//     implements Callable<DataTransferObject<GetJPAEntityInfoResponse>> {
-//   private final JavaCommandService javaCommandService;
-//
-//   @Option(names = "--cwd", description = "Current Working Directory", required = true)
-//   private Path cwd;
-//
-//   @Option(
-//       names = {"--file-path"},
-//       description = "The absolute path to the .java file.",
-//       required = true)
-//   private Path filePath;
-//
-//   @Option(
-//       names = "--ide",
-//       description = "The IDE the command is being called from.",
-//       required = true)
-//   private SupportedIDE ide = SupportedIDE.NONE;
-//
-//   @Override
-//   public DataTransferObject<GetJPAEntityInfoResponse> call() {
-//     return this.javaCommandService.getJPAEntityInfo(this.cwd, this.filePath, this.ide);
-//   }
-// }
+package io.github.syntaxpresso.core.command;
+
+import io.github.syntaxpresso.core.command.dto.GetJPAEntityInfoResponse;
+import io.github.syntaxpresso.core.common.DataTransferObject;
+import io.github.syntaxpresso.core.common.extra.SupportedIDE;
+import io.github.syntaxpresso.core.common.extra.SupportedLanguage;
+import io.github.syntaxpresso.core.service.java.command.GetJPAEntityInfoCommandService;
+import java.nio.file.Path;
+import java.util.concurrent.Callable;
+import lombok.RequiredArgsConstructor;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@RequiredArgsConstructor
+@Command(
+    name = "get-jpa-entity-info",
+    description = "Get necessary info of an specific Entity to create a JPA repository.")
+public class GetJPAEntityInfoCommand
+    implements Callable<DataTransferObject<GetJPAEntityInfoResponse>> {
+  private final GetJPAEntityInfoCommandService getJPAEntityInfoCommandService;
+
+  @Option(names = "--cwd", description = "Current Working Directory", required = true)
+  private Path cwd;
+
+  @Option(
+      names = {"--file-path"},
+      description = "The absolute path to the .java file.",
+      required = true)
+  private Path filePath;
+
+  @Option(
+      names = "--language",
+      description = "The language related to the command execution.",
+      required = true)
+  private SupportedLanguage language;
+
+  @Option(
+      names = "--ide",
+      description = "The IDE the command is being called from.",
+      required = true)
+  private SupportedIDE ide = SupportedIDE.NONE;
+
+  @Option(
+      names = "--superclass-source",
+      description = "Base64 encoded source code of the superclass if needed.",
+      required = false)
+  private String superclassSource;
+
+  @Override
+  public DataTransferObject<GetJPAEntityInfoResponse> call() {
+    if (SupportedLanguage.JAVA.equals(this.language)) {
+      return this.getJPAEntityInfoCommandService.run(
+          this.cwd, this.filePath, this.language, this.ide, this.superclassSource);
+    }
+    return DataTransferObject.error("Language not supported.");
+  }
+}
--- src/main/java/io/github/syntaxpresso/core/command/dto/GetJPAEntityInfoResponse.java
+++ src/main/java/io/github/syntaxpresso/core/command/dto/GetJPAEntityInfoResponse.java
@@ -1,8 +1,6 @@
 package io.github.syntaxpresso.core.command.dto;
 
 import java.io.Serializable;
-import java.util.List;
-import java.util.Map;
 import lombok.*;
 
 @Data
@@ -11,11 +9,9 @@ import lombok.*;
 @Builder
 public class GetJPAEntityInfoResponse implements Serializable {
   private Boolean isJPAEntity;
-  private String entityPath;
+  private String entityType;
   private String entityPackageName;
+  private String entityPath;
   private String idFieldType;
   private String idFieldPackageName;
-  private String recommendedRepositoryName;
-  private String recommendedRepositoryPackageName;
-  private List<Map<String, String>> recommendedTypes;
 }
