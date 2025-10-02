10466500d08a4d10051d2826ffd0f145754bf5ec
André Luís <zandreluisos@gmail.com>
2025-10-01 22:04:44 -0300

    perf: refactor GetMainClassCommandService with parallel processing
    - Convert nested loops to parallel stream processing with early termination
    - Extract processFileForMainClass and createMainClassResponse methods
    - Implement findAny() for efficient main class discovery
    - Improve performance for main class detection across large projects

--- src/main/java/io/github/syntaxpresso/core/service/java/command/GetMainClassCommandService.java
+++ src/main/java/io/github/syntaxpresso/core/service/java/command/GetMainClassCommandService.java
@@ -30,67 +30,81 @@ public class GetMainClassCommandService {
       return validateArguments;
     }
     List<TSFile> allTSFiles = this.javaLanguageService.getAllJavaFilesFromCwd(cwd);
-    for (TSFile tsFile : allTSFiles) {
-      Optional<TSNode> publicClassNode =
-          this.getJavaLanguageService().getClassDeclarationService().getPublicClass(tsFile);
-      if (publicClassNode.isEmpty()) {
-        continue;
-      }
-      List<TSNode> allClassMethodNodes =
+    Optional<DataTransferObject<GetMainClassResponse>> mainClassResult =
+        allTSFiles.parallelStream()
+            .map(tsFile -> this.processFileForMainClass(tsFile))
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .findAny();
+    return mainClassResult.orElse(
+        DataTransferObject.error("Couldn't find the main class of this project."));
+  }
+
+  private Optional<DataTransferObject<GetMainClassResponse>> processFileForMainClass(
+      TSFile tsFile) {
+    Optional<TSNode> publicClassNode =
+        this.getJavaLanguageService().getClassDeclarationService().getPublicClass(tsFile);
+    if (publicClassNode.isEmpty()) {
+      return Optional.empty();
+    }
+    List<TSNode> allClassMethodNodes =
+        this.getJavaLanguageService()
+            .getClassDeclarationService()
+            .getMethodDeclarationService()
+            .getAllMethodDeclarationNodes(tsFile, publicClassNode.get());
+    for (TSNode methodNode : allClassMethodNodes) {
+      boolean isMainMethod =
           this.getJavaLanguageService()
               .getClassDeclarationService()
               .getMethodDeclarationService()
-              .getAllMethodDeclarationNodes(tsFile, publicClassNode.get());
-      for (TSNode methodNode : allClassMethodNodes) {
-        boolean isMainMethod =
-            this.getJavaLanguageService()
-                .getClassDeclarationService()
-                .getMethodDeclarationService()
-                .isMainMethod(tsFile, methodNode);
-        if (isMainMethod) {
-          Optional<TSNode> mainClassNameNode =
-              this.getJavaLanguageService()
-                  .getClassDeclarationService()
-                  .getClassDeclarationNameNode(tsFile, publicClassNode.get());
-          if (mainClassNameNode.isEmpty()) {
-            return DataTransferObject.error(
-                "Couldn't obtain public class' name from "
-                    + tsFile.getFile().getAbsolutePath().toString()
-                    + ".");
-          }
-          String mainClassName = tsFile.getTextFromNode(mainClassNameNode.get());
-          Optional<TSNode> mainClassPackageNode =
-              this.getJavaLanguageService()
-                  .getPackageDeclarationService()
-                  .getPackageDeclarationNode(tsFile);
-          if (mainClassPackageNode.isEmpty()) {
-            return DataTransferObject.error(
-                "Couldn't obtain public class' package name from "
-                    + tsFile.getFile().getAbsolutePath().toString()
-                    + ".");
-          }
-          Optional<TSNode> mainClassPackageScopeNode =
-              this.getJavaLanguageService()
-                  .getPackageDeclarationService()
-                  .getPackageScopeNode(tsFile, mainClassPackageNode.get());
-          if (mainClassPackageScopeNode.isEmpty()) {
-            return DataTransferObject.error(
-                "Couldn't obtain public class' package scope from "
-                    + tsFile.getFile().getAbsolutePath().toString()
-                    + ".");
-          }
-          String mainClassPackageScopeName =
-              tsFile.getTextFromNode(mainClassPackageScopeNode.get());
-          GetMainClassResponse response =
-              GetMainClassResponse.builder()
-                  .filePath(tsFile.getFile().getAbsolutePath())
-                  .className(mainClassName)
-                  .packageName(mainClassPackageScopeName)
-                  .build();
-          return DataTransferObject.success(response);
-        }
+              .isMainMethod(tsFile, methodNode);
+      if (isMainMethod) {
+        return Optional.of(this.createMainClassResponse(tsFile, publicClassNode.get()));
       }
     }
-    return DataTransferObject.error("Couldn't find the main class of this project.");
+    return Optional.empty();
+  }
+
+  private DataTransferObject<GetMainClassResponse> createMainClassResponse(
+      TSFile tsFile, TSNode publicClassNode) {
+    Optional<TSNode> mainClassNameNode =
+        this.getJavaLanguageService()
+            .getClassDeclarationService()
+            .getClassDeclarationNameNode(tsFile, publicClassNode);
+    if (mainClassNameNode.isEmpty()) {
+      return DataTransferObject.error(
+          "Couldn't obtain public class' name from "
+              + tsFile.getFile().getAbsolutePath().toString()
+              + ".");
+    }
+    String mainClassName = tsFile.getTextFromNode(mainClassNameNode.get());
+    Optional<TSNode> mainClassPackageNode =
+        this.getJavaLanguageService()
+            .getPackageDeclarationService()
+            .getPackageDeclarationNode(tsFile);
+    if (mainClassPackageNode.isEmpty()) {
+      return DataTransferObject.error(
+          "Couldn't obtain public class' package name from "
+              + tsFile.getFile().getAbsolutePath().toString()
+              + ".");
+    }
+    Optional<TSNode> mainClassPackageScopeNode =
+        this.getJavaLanguageService()
+            .getPackageDeclarationService()
+            .getPackageScopeNode(tsFile, mainClassPackageNode.get());
+    if (mainClassPackageScopeNode.isEmpty()) {
+      return DataTransferObject.error(
+          "Couldn't obtain public class' package scope from "
+              + tsFile.getFile().getAbsolutePath().toString()
+              + ".");
+    }
+    String mainClassPackageScopeName = tsFile.getTextFromNode(mainClassPackageScopeNode.get());
+    GetMainClassResponse response =
+        GetMainClassResponse.builder()
+            .filePath(tsFile.getFile().getAbsolutePath())
+            .className(mainClassName)
+            .packageName(mainClassPackageScopeName)
+            .build();
+    return DataTransferObject.success(response);
   }
 }
